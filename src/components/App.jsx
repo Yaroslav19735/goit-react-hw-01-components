b604c6684303264fdc225826fcd9afab5a122c03
Yaroslav19735 <yarkela1997@gmail.com>
2022-12-19 15:01:18 +0200

    com

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,11 @@
 import { Profile } from "components/Profile/Profile";
+import { Statistics } from "components//Statistics/Statistics";
+import { FriendList } from "components//FriendList/FriendList";
+import { TransactionHistory } from "components/TransactionHistory/TransactionHistory";
 import user from './data/user';
+import data from './data/data';
+import friends from './data/friends';
+import transactions from './data/transactions';
 
 export const App = () => {
   return (
@@ -20,6 +26,10 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
